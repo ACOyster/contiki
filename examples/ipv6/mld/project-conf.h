60e6d98ec0d9e1bf32e050f93b190c70fef0c4b1
James Hart <acesoyster@gmail.com>
2017-01-25 15:15:50 +0000

    Additions to makefile to ensure slip functionality

--- examples/ipv6/mld/project-conf.h
+++ examples/ipv6/mld/project-conf.h
@@ -68,4 +68,29 @@
 /*Set up MLD*/
 #define UIP_CONF_MLD                  1
 
+#ifndef WITH_NON_STORING
+#define WITH_NON_STORING 0 /* Set this to run with non-storing mode */
+#endif /* WITH_NON_STORING */
+
+#if WITH_NON_STORING
+#undef RPL_NS_CONF_LINK_NUM
+#define RPL_NS_CONF_LINK_NUM 40 /* Number of links maintained at the root */
+#undef UIP_CONF_MAX_ROUTES
+#define UIP_CONF_MAX_ROUTES 0 /* No need for routes */
+#undef RPL_CONF_MOP
+#define RPL_CONF_MOP RPL_MOP_NON_STORING /* Mode of operation*/
+#endif /* WITH_NON_STORING */
+
+#ifndef QUEUEBUF_CONF_NUM
+#define QUEUEBUF_CONF_NUM          4
+#endif
+
+#ifndef UIP_CONF_BUFFER_SIZE
+#define UIP_CONF_BUFFER_SIZE    140
+#endif
+
+#ifndef UIP_CONF_RECEIVE_WINDOW
+#define UIP_CONF_RECEIVE_WINDOW  60
+#endif
+
 #endif /* PROJECT_CONF_H_ */
